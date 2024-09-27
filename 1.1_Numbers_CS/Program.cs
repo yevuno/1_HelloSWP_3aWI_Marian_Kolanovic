690fa6999e54081cfc4e5fa7fd9486f322e104cf
yevuno <mari_kola@icloud.com>
2024-09-27 11:13:38 +0200

    Finished second assignment

---
+++ 1.1_Numbers_CS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input;
+
+        Console.WriteLine("Input:");
+
+        while (true)
+        {
+            input = Console.ReadLine();
+
+            if (input.Equals("Beenden", StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+            else if (int.TryParse(input, out int number))
+            {
+                string typeOfNumber = number.GetType().ToString();
+                Console.WriteLine($"Eingegebene Nummer: {number}, \n Datentyp: {typeOfNumber} \n\n'Beenden' zum Beenden.");
+
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("Eingabe ist keine Nummer. Bitte erneut eingeben.");
+                continue;
+            }
+
+
+
+            Console.WriteLine($"Eingabe abgeschlossen: {input}");
+        }
+
+        Console.WriteLine("Ende.");
+    }
+}
