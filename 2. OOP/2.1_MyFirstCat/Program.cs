d615b0c13176a32346e05f3b9bffd4a5ff33cde4
yevuno <mari_kola@icloud.com>
2024-11-08 13:20:20 +0100

    Cat Programm Finished

--- 2. OOP/2.1_MyFirstCat/Program.cs
+++ 2. OOP/2.1_MyFirstCat/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+namespace _2._1_MyFirstCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCat = new Cat { Color = "Schwarz", Birthdate = new DateTime(2008, 1, 23) };
+            myCat.CalculateAge();
+
+            Console.WriteLine($"Die Katze hat die Farbe: {myCat.Color}");
+            Console.WriteLine($"Die Katze ist {myCat.Age} Jahre alt.");
+        }
+    }
+}
