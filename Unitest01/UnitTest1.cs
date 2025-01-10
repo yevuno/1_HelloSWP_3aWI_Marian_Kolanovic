260fefc751d08029a1292de944b17ff77c57cfad
yevuno <mari_kola@icloud.com>
2025-01-10 11:38:28 +0100

    Test finished

--- Unitest01/UnitTest1.cs
+++ Unitest01/UnitTest1.cs
@@ -1,15 +1,63 @@
-﻿namespace Unitest01;
+﻿using SchuleVerwaltung;
 
-public class Tests
+
+namespace Unitest01
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class SchuleTests
     {
-    }
+        private SchuleVerwaltung.Schule schule;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [SetUp]
+        public void Setup()
+        {
+            schule = new Schule();
+            schule.SchuelerListe.Add(new Schueler("maennlich", new DateTime(2008, 1, 23), "3aWI"));
+            schule.SchuelerListe.Add(new Schueler("weiblich", new DateTime(2007, 11, 11), "3aWI"));
+            schule.KlassenraumListe.Add(new Klassenraum("247", 30, false));
+            schule.KlassenraumListe.Add(new Klassenraum("185", 25, true));
+        }
+
+        [Test]
+        public void TestAnzahlSchueler()
+        {
+            var anzahl = schule.AnzahlSchueler();
+            Assert.That(anzahl, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestAnzahlMaennlicheSchueler()
+        {
+            var anzahl = schule.AnzahlMaennlicheSchueler();
+            Assert.That(anzahl, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestDurchschnittsalter()
+        {
+            var durchschnittsalter = schule.Durchschnittsalter();
+            Assert.That(durchschnittsalter, Is.EqualTo(17.08).Within(0.1));
+        }
+
+        [Test]
+        public void TestRaeumeMitCynap()
+        {
+            var raeumeMitCynap = schule.RaeumeMitCynap();
+            Assert.That(raeumeMitCynap.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestFrauenanteilInKlasse()
+        {
+            var frauenanteil = schule.FrauenanteilInKlasse("3aWI");
+            Assert.That(frauenanteil, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void TestKannRaumFuerKlasseUnterrichten()
+        {
+            var kannUnterrichtetWerden = schule.KannRaumFuerKlasseUnterrichten("3aWI", "185");
+            Assert.That(kannUnterrichtetWerden, Is.True);
+        }
     }
 }
