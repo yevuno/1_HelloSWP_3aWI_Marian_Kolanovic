32ca0c4e8d4d60012d43aaf814ca77bc3ddfdb88
yevuno <mari_kola@icloud.com>
2025-01-10 11:53:24 +0100

    Test fertig

---
+++ Test_10.01.2025/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace SchuleVerwaltung
+
+{
+    public class Person
+    {
+        public string Geschlecht { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+
+        public Person(string geschlecht, DateTime geburtsdatum)
+        {
+            Geschlecht = geschlecht;
+            Geburtsdatum = geburtsdatum;
+        }
+    }
+}
