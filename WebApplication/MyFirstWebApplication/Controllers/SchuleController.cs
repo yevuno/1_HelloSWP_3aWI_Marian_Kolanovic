684c62d8aeb7ec7ad671e17c3ac2020731421451
yevuno <mari_kola@icloud.com>
2025-03-28 10:05:46 +0100

    Add initial controllers and configuration files for MyFirstWebApplication

---
+++ WebApplication/MyFirstWebApplication/Controllers/SchuleController.cs
@@ -0,0 +1,6 @@
+namespace _01_MyFirstWebApplication;
+
+public interface SchuleController
+{
+    
+}
---
+++ WebApplication/MyFirstWebApplication/Controllers/WeatherForecast.cs
@@ -0,0 +1,6 @@
+namespace _01_MyFirstWebApplication.Controllers;
+
+public interface WeatherForecast
+{
+    
+}
